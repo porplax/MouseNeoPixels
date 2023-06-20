84b2952dc1c4981a7ed35ca26860a95ffecea853
Zayne Marsh <66521670+porplax@users.noreply.github.com>
2023-06-19 23:59:08 -0400

    Update code.py

--- code.py
+++ code.py
@@ -6,22 +6,22 @@ import json
 import neopixel
 import board
 import sys
-
+import math
 PIXEL_PIN = board.GP16
 NUMBER_OF_PIXELS = 30
 ORDER = neopixel.GRB
 
 neo = neopixel.NeoPixel(
-    PIXEL_PIN, NUMBER_OF_PIXELS, brightness=0.2, auto_write=False, pixel_order=ORDER)
+    PIXEL_PIN, NUMBER_OF_PIXELS, brightness=0.5, auto_write=False, pixel_order=ORDER)
 neo.fill((0, 0, 0))
 neo.show()
 print("listening...")
 serial = sys.stdin
 
 offset = -1
-height = 255
+height = 0
 while True:
-    neo.fill((height, 0, 0))
+    neo.fill((10, 10, 10))
     if supervisor.runtime.serial_bytes_available:
         data_in = serial.readline()
         data = None
@@ -33,6 +33,7 @@ while True:
 
         if isinstance(data, dict):
             if "x" in data:
+                
                 try:
                     neo[int(data["x"]/64)-(2-offset)] = (64, 64, 64)
                     neo[int(data["x"]/64)-(1-offset)] = (127, 127, 127)
@@ -46,7 +47,8 @@ while True:
                     height = int(data["key"])
                 except:
                     pass
-    height += (0 - height) * 0.05
-
     neo.show()
+
+    height += .01
+    
     time.sleep(0.001)
